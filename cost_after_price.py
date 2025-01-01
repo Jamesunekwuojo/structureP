c29941704cc7ab0fbc2cadcb4bfa8a778b48228c
Jamesunekwuojo <unekwuojoabah4@gmail.com>
2025-01-01 03:25:09 +0100

    wrote code for the cost after tax ass, working partially fine

--- cost_after_price.py
+++ cost_after_price.py
@@ -11,3 +11,18 @@ step 4: return cost in the 'cost_after_price' function
 
 step 5: call by value the 'cost_after_price' function with arguments 100 and 10
 """
+
+# Python code implementation of the above steps
+def cost_after_tax(price, tax):
+
+    cost = price * (1 + tax / 100)
+
+    print(f"cost after after tax is {cost}")
+
+    return cost
+
+def main():
+
+    cost_after_tax(100, 10)
+
+main()

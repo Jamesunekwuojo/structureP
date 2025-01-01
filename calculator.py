c888e9085395129967ad9e0fe62bf65d6f173f57
Jamesunekwuojo <unekwuojoabah4@gmail.com>
2025-01-01 03:18:46 +0100

    pseudocode created for both assignment questions

---
+++ calculator.py
@@ -0,0 +1,38 @@
+
+
+"""
+pseudocode
+
+step 1: Define a function to  declare two variables a and b and  allow dynamic input by uses
+
+
+step 2: Create functions called: addittion, multiplication, division, and subtraction  to perform any of the arithmetic operation
+
+step 3: Crete a memo and ask user to choose operation to perform.
+
+1 - Addition
+2 - Subtraction
+3 - Multiplication
+4 - Division
+
+step 4 : Print out result
+
+step 5: Create variable 'repeat_operation' and assign it a boolean value of 'false'
+
+step 6: Create another variable named controller_operation  and dynamically assign a value of '1' or '0', Asking "Do you want to perform another operation? (yes/no) yes- press 1, No- press 0"
+
+step 7: if controller_operation is equal to 1 then repeat_operation is set to true, 
+
+ELSE IF repeat_operation is set to false
+ELSE print "Enter a valid number " and end the program
+
+step 8: while repeat_operation is equal to true repeat the following steps; 1 to 4.
+
+step 9: while repeat_operation is equal to false print "Thank you for using our calculator" and end the program
+
+"""
+
+
+
+# Python code to implement the above pseudocode
+
---
+++ cost_after_price.py
@@ -0,0 +1,13 @@
+"""
+pseudocode
+
+step 1: define 'main()' f=module function
+
+step 2: Define user define function 'def cost_after_price(price, tax):' 
+
+step 3: Declare varibale for cost = Price * ( 1 + tax / 100 )
+
+step 4: return cost in the 'cost_after_price' function
+
+step 5: call by value the 'cost_after_price' function with arguments 100 and 10
+"""
